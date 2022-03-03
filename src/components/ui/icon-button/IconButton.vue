832b5ebefc01c28192162f1b245c2daabc118505
Maicol Bruski <maicolbruski@gmail.com>
2022-03-03 10:59:44 -0300

    add IconButton

---
+++ src/components/ui/icon-button/IconButton.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import Icon from '../icon/Icon.vue'
+// import UiButton from '../button/Button.vue'
+
+interface Props {
+	icon: string
+	variant: string
+	size: string
+	type: string
+	fab: boolean
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+	<ui-button class="ui-icon-button" :variant="variant" :class="{ '-fab': fab }" :size="size">
+		<icon :name="icon" :type="type" class="icon" />
+	</ui-button>
+</template>
+
+<style lang="scss">
+@import './IconButton.scss';
+</style>
