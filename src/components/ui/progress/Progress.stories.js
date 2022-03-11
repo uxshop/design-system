d360491d7d1bc61dbcfa4b559bbb03bccc828627
vitorbruski <vitor@dooca.com.br>
2022-03-11 14:33:18 -0300

    PascalCase files

--- src/components/ui/progress/Progress.stories.js
+++ src/components/ui/progress/Progress.stories.js
@@ -1,25 +1,25 @@
-import UiProgress from './Progress.vue'
+import Progress from './Progress.vue'
 
 // More on default export: https://storybook.js.org/docs/vue/writing-stories/introduction#default-export
 export default {
-  title: 'Ui/Progress',
-  component: UiProgress,
-  argTypes: {},
+	title: 'Ui/Progress',
+	component: Progress,
+	argTypes: {}
 }
 
 // More on component templates: https://storybook.js.org/docs/vue/writing-stories/introduction#using-args
 const Template = (args) => ({
-  components: { UiProgress },
-  setup() {
-    return { args }
-  },
-  template: `
-    <UiProgress v-bind="args" />
-  `,
+	components: { Progress },
+	setup() {
+		return { args }
+	},
+	template: `
+    <Progress v-bind="args" />
+  `
 })
 
 export const Default = Template.bind({})
 Default.args = {
-  value:"70" ,
-  max:"100"
+	value: '70',
+	max: '100'
 }
