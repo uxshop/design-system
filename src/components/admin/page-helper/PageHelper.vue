50ab62a2b75c75b555ad2ae6a6e5f2c697995253
vitorbruski <vitor@dooca.com.br>
2022-04-07 18:47:24 -0300

    Alert + pageHelper changes

--- src/components/admin/page-helper/PageHelper.vue
+++ src/components/admin/page-helper/PageHelper.vue
@@ -1,13 +1,19 @@
 <script setup lang="ts">
 import Icon from '../../ui/icon/Icon.vue'
+
+defineProps({
+	helperLink: String,
+	helperName: String
+})
 </script>
 
 <template>
 	<div class="ui-page-helper">
 		<icon name="info" />
-		<div class="ui-page-helper-text">
-			<slot />
-		</div>
+		<span class="ui-page-helper-text">
+			<b>Precisa de ajuda?</b> Acesse o material sobre
+			<b><a :href="helperLink" class="ui-page-helper-name" v-text="helperName" /></b>
+		</span>
 	</div>
 </template>
 
