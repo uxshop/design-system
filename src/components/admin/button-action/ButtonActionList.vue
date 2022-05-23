d4c90915651a8c205d59972b3988a3f09cab4eba
Maicol Bruski <maicolbruski@gmail.com>
2022-05-23 15:26:48 -0300

    fix button action list

---
+++ src/components/admin/button-action/ButtonActionList.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup></script>
+
+<template>
+	<div class="button-action-list">
+		<slot />
+	</div>
+</template>
+
+<style lang="scss">
+.button-action-list {
+	display: flex;
+	gap: 8px;
+	white-space: nowrap;
+	display: flex;
+	align-items: center;
+	justify-content: flex-end;
+}
+</style>
