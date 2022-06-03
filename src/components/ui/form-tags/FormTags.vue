6ce343af14b029d95f22f4383310f7033281ab28
Maicol Bruski <maicolbruski@gmail.com>
2022-06-03 00:23:00 -0300

    fix

--- src/components/ui/form-tags/FormTags.vue
+++ src/components/ui/form-tags/FormTags.vue
@@ -74,7 +74,7 @@ const init = () => {
 
 		const el = document.querySelector(`#${uid}`)
 		if (el) {
-			element.value = new Choices(el, getSettings())
+			element.value = window.Choices(el, getSettings())
 
 			el.addEventListener(
 				'change',
