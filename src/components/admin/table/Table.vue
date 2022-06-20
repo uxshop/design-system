ffd6a728c81e79185e459161c0a2aab9fd4a22db
Maicol Bruski <maicolbruski@gmail.com>
2022-06-20 13:34:01 -0300

    update table

--- src/components/admin/table/Table.vue
+++ src/components/admin/table/Table.vue
@@ -3,11 +3,12 @@ defineEmits(['click'])
 defineProps<{
 	hover?: boolean
 	noPadding?: boolean
+	tiny?: boolean
 }>()
 </script>
 <template>
 	<div class="table-responsive">
-		<div class="ui-table" :class="{ '-hover': hover, '-no-padding': noPadding }">
+		<div class="ui-table" :class="{ '-hover': hover, '-no-padding': noPadding, '-tiny': tiny }">
 			<div v-if="$slots.header" class="ui-table-header">
 				<div class="ui-table-row -head">
 					<slot name="header" />
