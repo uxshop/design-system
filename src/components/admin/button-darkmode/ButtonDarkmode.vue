35564382bc1fcfedc67604bc4de337f78571afa7
Maicol Bruski <maicolbruski@gmail.com>
2022-06-21 18:39:36 -0300

    button

--- src/components/admin/button-darkmode/ButtonDarkmode.vue
+++ src/components/admin/button-darkmode/ButtonDarkmode.vue
@@ -3,29 +3,27 @@ import { onMounted, ref } from 'vue'
 import IconButton from '../../ui/icon-button/IconButton.vue'
 
 const iconMode = ref('dark_mode')
+const colorScheme = 'adm_color_scheme'
+const system = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
 
-const setDarkmode = (val: boolean) => {
-	if (val) {
-		localStorage.setItem('darkmode', '1')
-		document.documentElement.setAttribute('data-theme', 'dark')
+const setDarkmode = (val: any) => {
+	localStorage.setItem(colorScheme, val)
+	document.documentElement.setAttribute('data-theme', val)
+
+	if (val == 'dark') {
 		iconMode.value = 'light_mode'
 	} else {
-		document.documentElement.removeAttribute('data-theme')
-		localStorage.removeItem('darkmode')
 		iconMode.value = 'dark_mode'
 	}
 }
 
 const toggleDarkmode = () => {
-	if (localStorage.getItem('darkmode') == '1') {
-		setDarkmode(false)
-	} else {
-		setDarkmode(true)
-	}
+	const color = localStorage.getItem(colorScheme) == 'dark' ? 'light' : 'dark'
+	setDarkmode(color)
 }
 
 onMounted(() => {
-	setDarkmode(localStorage.getItem('darkmode') != undefined)
+	setDarkmode(localStorage.getItem(colorScheme) ?? system)
 })
 </script>
 
