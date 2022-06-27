e0dd1bb611e5f3d8ba831e58b0c7df60385cc99b
Maicol Bruski <maicolbruski@gmail.com>
2022-06-27 11:31:50 -0300

    ajustes ds

---
+++ src/components/ui/form-layout/FormLayoutGroup.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="ui-form-layout-group">
+		<slot />
+	</div>
+</template>
+
+<style lang="scss">
+.ui-form-layout-group {
+	display: flex;
+	gap: var(--grid-gutter-width);
+
+	> div {
+		flex-grow: 1;
+	}
+}
+</style>
---
+++ src/components/ui/form-layout/FormLayoutItem.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+defineProps<{
+	group?: boolean
+}>()
+</script>
+<template>
+	<div class="ui-form-layout-item" :class="{ '-group': group }">
+		<slot />
+	</div>
+</template>
+
+<style lang="scss">
+.ui-form-layout-item {
+	+ .ui-form-layout-item {
+		margin-top: var(--form-group-margin-bottom);
+	}
+
+	// > div {
+	// 	margin-top: var(--form-group-margin-bottom);
+	// }
+
+	&.-group {
+		display: flex;
+		gap: var(--grid-gutter-width);
+	}
+}
+</style>
