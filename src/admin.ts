3df47eee03d3f52e62a53e4133e93f665cb756bd
Maicol Bruski <maicolbruski@gmail.com>
2022-07-01 10:36:50 -0300

    fix import

--- src/admin.ts
+++ src/admin.ts
@@ -17,7 +17,7 @@ import CardActive from './components/admin/card-active/CardActive.vue'
 import StatsGroup from './components/admin/stats/StatsGroup.vue'
 import Codemirror from './components/admin/codemirror/Codemirror.vue'
 import Savebar from './components/admin/savebar/Savebar.vue'
-import Userbar from './components/admin/userbar/Userbar.vue'
+import Topbar from './components/admin/topbar/Topbar.vue'
 import Seo from './components/admin/seo/Seo.vue'
 import CardSeo from './components/admin/card-seo/CardSeo.vue'
 
@@ -46,7 +46,7 @@ export default {
 		app.component(`${prefix}StatsGroup`, StatsGroup)
 		app.component(`${prefix}Codemirror`, Codemirror)
 		app.component(`${prefix}Savebar`, Savebar)
-		app.component(`${prefix}Userbar`, Userbar)
+		app.component(`${prefix}Topbar`, Topbar)
 		app.component(`${prefix}Seo`, Seo)
 		app.component(`${prefix}CardSeo`, CardSeo)
 	}
