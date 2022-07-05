4f77b4d7a3eb4c88f402817c86734b3135ce8b4d
simaomeyrerdooca <simao@doocacommerce.com.br>
2022-07-05 13:50:10 -0300

    fix: condições da prop selectType

--- src/components/admin/browser-select/BrowserSelect.vue
+++ src/components/admin/browser-select/BrowserSelect.vue
@@ -155,8 +155,6 @@ const pushToMemoryList = (items: any[]) => {
 }
 
 const updateByModal = ({ memoryList, ids }: any) => {
-	console.log(memoryList, ids)
-
 	searchBy.value = null
 	pushToMemoryList(memoryList)
 	if (ids) {
@@ -203,18 +201,19 @@ watch(
 	},
 	{ deep: true, immediate: true }
 )
+defineExpose({ onClickSearch })
 </script>
 
 <template>
 	<div class="ui-browser-select">
-		<div class="ui-browser-select-button">
-			<slot name="button" v-if="selectType == 'btn' && !hideBtn">
+		<div class="ui-browser-select-button" v-if="selectType == 'btn' && !hideBtn">
+			<slot name="button">
 				<div class="area-select" @click="onClickSearch" :class="{ disabled: limit > 0 && rows.length == limit }">
 					<span>{{ placeholder }}</span>
 				</div>
 			</slot>
 		</div>
-		<div>
+		<div v-else>
 			<div class="ui-browser-select-input">
 				<Row>
 					<Col>
