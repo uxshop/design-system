65c84b7905b5c7d76759ac0f79b7b0c6ba064b62
vitorbruski <vitor@dooca.com.br>
2022-07-21 08:54:39 -0300

    image removed

--- src/components/admin/media-card/MediaCard.vue
+++ src/components/admin/media-card/MediaCard.vue
@@ -26,9 +26,6 @@ if (props.inverse) {
 if (props.video) {
 	mediaCardClass.value.push('-video')
 }
-
-const image =
-	'https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Zm9jdXN8ZW58MHx8MHx8&w=1000&q=80'
 </script>
 
 <template>
