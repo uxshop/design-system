dd945f78f55262b9376a4df55d4c6e6d9b6cbf0c
joao.lima08 <joao.lima@bagy.com.br>
2023-01-04 12:09:45 -0300

    refactor: refatora estilização Breadcrumb

--- src/components/ui/breadcrumb/UiBreadcrumb.vue
+++ src/components/ui/breadcrumb/UiBreadcrumb.vue
@@ -1,5 +1,17 @@
+<script setup lang="ts">
+import Icon from '../../ui/icon/Icon.vue'
+withDefaults(
+	defineProps<{
+		to: object
+		icon?: boolean
+	}>(),
+	{ icon: false }
+)
+</script>
+
 <template>
 	<div class="ui-breadcrumb">
+		<Icon v-if="icon" name="arrow_back" class="-icon" />
 		<slot />
 	</div>
 </template>
