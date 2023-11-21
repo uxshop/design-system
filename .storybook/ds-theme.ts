c453f14e58edd87c1d1fa84436b18a6b44665c3c
tiagotermedooca <tiago.terme@bagy.com.br>
2023-11-21 08:24:14 -0300

    fix: logo e theme

--- .storybook/ds-theme.ts
+++ .storybook/ds-theme.ts
@@ -3,38 +3,8 @@
 import { create } from '@storybook/theming/create';
 
 export default create({
-  base: 'dark',
-  // Typography
-  // fontBase: 'BlinkMacSystemFont, "San Francisco", "Segoe UI", Roboto, "Helvetica Neue", sans-serif',
-  // fontCode: 'monospace',
-
+  base: 'light',
   brandTitle: 'Bagy Design System',
-  // brandUrl: 'https://bagy.com.br',
   brandImage: './logo.svg',
-  // brandTarget: '_self',
-
-  //
-  // colorPrimary: '#3A10E5',
-  colorSecondary: '#DC189B',
-
-  // UI
-  // appBg: '#ffffff',
-  // appContentBg: '#ffffff',
-  // appBorderColor: '#585C6D',
-  // appBorderRadius: 4,
-
-  // Text colors
-  // textColor: '#10162F',
-  // textInverseColor: '#ffffff',
-
-  // Toolbar default and active colors
-  // barTextColor: '#9E9E9E',
-  // barSelectedColor: '#585C6D',
-  // barBg: '#ffffff',
-
-  // Form colors
-  // inputBg: '#ffffff',
-  // inputBorder: '#10162F',
-  // inputTextColor: '#10162F',
-  // inputBorderRadius: 2,
+  colorSecondary: '#DC189B'
 });
