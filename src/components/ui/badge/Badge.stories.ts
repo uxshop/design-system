1b1d2a6aa9c3491ed80d915a940401b2c57f02af
vitorbruski <vitorgbruski@gmail.com>
2023-11-24 13:59:39 -0300

    fix: removido codigo comentado

--- src/components/ui/badge/Badge.stories.ts
+++ src/components/ui/badge/Badge.stories.ts
@@ -2,11 +2,9 @@ import type { Meta, StoryObj } from '@storybook/vue3'
 
 import Badge from './Badge.vue'
 
-// More on how to set up stories at: https://storybook.js.org/docs/vue/writing-stories/introduction
 const meta = {
 	title: 'Example/Badge',
 	component: Badge,
-	// This component will have an automatically generated docsPage entry: https://storybook.js.org/docs/vue/writing-docs/autodocs
 	tags: ['autodocs'],
 	args: {
 		label: 'Badge label',
@@ -31,11 +29,7 @@ const meta = {
 
 export default meta
 type Story = StoryObj<typeof meta>
-/*
- *👇 Render functions are a framework specific feature to allow you control on how the component renders.
- * See https://storybook.js.org/docs/vue/api/csf
- * to learn how to use render functions.
- */
+
 export const Default: Story = {
 	args: {}
 }
