f074cad75c88f7400b73137b58b0145617dea078
vitorbruski <vitorgbruski@gmail.com>
2023-12-01 00:19:37 -0300

    feat: adicionado novos tipos globais

--- src/types/ITypes.ts
+++ src/types/ITypes.ts
@@ -1,1 +1,7 @@
 export type IVariant = 'primary' | 'secondary' | 'success' | 'danger' | 'link' | 'dark' | 'plain'
+
+export type IIconTypes = 'outlined' | 'rounded' | 'sharp'
+
+export type ISize = 'sm' | 'md' | 'lg'
+
+export type ITarget = '_blank' | '_self'
