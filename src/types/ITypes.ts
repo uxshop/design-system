8d7b7a6cfe0473337a21e690ee8a58d06146967b
vitorbruski <vitorgbruski@gmail.com>
2023-12-01 09:36:56 -0300

    refactor: alteraçao de nomenclaturas e correçao de imports

--- src/types/ITypes.ts
+++
@@ -1,9 +0,0 @@
-export type IVariant = 'default' | 'primary' | 'warning' | 'danger' | 'success'
-
-export type IDestructVariant = 'success' | 'danger' | 'primary'
-
-export type IIconTypes = 'outlined' | 'rounded' | 'sharp'
-
-export type ISize = 'sm' | 'md' | 'lg'
-
-export type ITarget = '_blank' | '_self'
