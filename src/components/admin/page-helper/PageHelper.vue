7302f7825ce77c5d6de27d7627aaf4db95bfddd1
Lima08 <joaopaulo.gomeslima8@gmail.com>
2023-12-12 22:57:58 -0300

    feat(Atualização do componente Page - PageHelper) Adiicona emiter

--- src/components/admin/page-helper/PageHelper.vue
+++ src/components/admin/page-helper/PageHelper.vue
@@ -1,18 +1,26 @@
 <script setup lang="ts">
 import Icon from '../../ui/icon/Icon.vue'
 
-defineProps({
-	helperLink: String,
-	helperName: String
-})
+export interface IPageHelper {
+	helperName: string
+	helperLink?: string
+}
+
+defineProps<IPageHelper>()
+const emit = defineEmits<{
+	(event: 'clicked'): void
+}>()
 </script>
 
 <template>
-	<div class="ui-page-helper">
+	<div class="ui-page-helper" @click="emit('clicked')">
 		<icon name="info" />
 		<span class="ui-page-helper-text">
 			<b>Precisa de ajuda?</b> Acesse o material sobre
-			<b><a :href="helperLink" class="ui-page-helper-name" v-text="helperName" target="_blank" /></b>
+			<b v-if="!!helperLink">
+				<a :href="helperLink" class="ui-page-helper-name" v-text="helperName" target="_blank" />
+			</b>
+			<b v-else class="text-lowercase">{{ helperName }}</b>
 		</span>
 	</div>
 </template>
