d6c585f3bc64fd94c76091589571a8b48face293
simaomeyrer <simaomeyrer@gmail.com>
2023-12-20 09:48:34 -0300

    fix: remoção da dependência do router

--- src/components/admin/sidebar/types.ts
+++ src/components/admin/sidebar/types.ts
@@ -1,14 +1,27 @@
 export interface Node {
 	name: string
 	to: string
-	permissions: string
+	permissions?: string
+	icon: string
+	novelty?: boolean
+	active?: boolean
+	disabled?: boolean
+	spacer?: boolean
+	last?: boolean
 }
 
 export interface SideBarItem {
 	name: string
 	to: string
-	permissions: string
+	permissions?: string
 	section?: string
 	icon?: string
+	caption?: string
 	nodes?: Node[]
+	novelty?: boolean
+	active?: boolean
+	disabled?: boolean
+	spacer?: boolean
+	last?: boolean
+	dropdown?: boolean
 }
