44af17635e7aae6bfd96514d9ed0a4b810381727
Lima08 <joaopaulo.gomeslima8@gmail.com>
2023-12-26 13:23:29 -0300

    refactor: remove computed do icon button

--- src/components/ui/icon-button/IconButton.vue
+++ src/components/ui/icon-button/IconButton.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { computed } from 'vue'
 import Icon from '../icon/Icon.vue'
 import Button from '../button/Button.vue'
 import type { IconType, Size } from 'src/types'
@@ -10,31 +9,18 @@ export interface Props {
 	size?: Size
 	type: IconType
 	fab?: boolean
-  disabled?: boolean
+	disabled?: boolean
 }
 
 const props = withDefaults(defineProps<Props>(), {
-	type: 'outlined'
+	type: 'outlined',
+	size: 'md'
 })
-
-const computedButtonProps = computed(() => ({
-	icon: props.icon,
-	variant: props.variant,
-	size: props.size,
-	type: props.type,
-	fab: props.fab,
-  disabled: props.disabled
-}))
 </script>
 
 <template>
-	<Button
-		class="ui-icon-button"
-		:variant="computedButtonProps.variant"
-		:class="{ '-fab': computedButtonProps.fab }"
-    :disabled="computedButtonProps.disabled"
-		:size="computedButtonProps.size">
-		<Icon :name="computedButtonProps.icon" :type="computedButtonProps.type" />
+	<Button class="ui-icon-button" :variant="variant" :class="{ '-fab': fab }" :size="size" :disabled="disabled">
+		<Icon :name="icon" :type="type" />
 	</Button>
 </template>
 
