97f2b02b0dbaeb1de550fd9385ed30e0af793050
Lima08 <joaopaulo.gomeslima8@gmail.com>
2023-12-26 15:28:56 -0300

    feat: atualiza icon removendo os tipos que não são utilizados e passando para 16 o size default

--- src/types/Types.ts
+++ src/types/Types.ts
@@ -2,7 +2,6 @@ export type Variant = 'default' | 'highlight' | 'warning' | 'success' | 'critica
 
 export type DestructVariant = 'success' | 'danger' | 'highlight'
 
-export type IconType = 'outlined' | 'rounded' | 'sharp' | 'filled' | 'rounded' | 'outlined' | 'twotone'
 export type Size = 'sm' | 'md' | 'lg'
 
 export type Target = '_blank' | '_self'
