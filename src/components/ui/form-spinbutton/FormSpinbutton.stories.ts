f36ab642641e6cff998eefd377cb826ce62f3454
Lima08 <joaopaulo.gomeslima8@gmail.com>
2024-01-22 16:56:36 -0300

    refactor: ajusta css, stories e componente formSpinButton

---
+++ src/components/ui/form-spinbutton/FormSpinbutton.stories.ts
@@ -0,0 +1,51 @@
+import { ref } from 'vue'
+import FormSpinbutton from './FormSpinbutton.vue'
+import type { Meta, StoryObj } from '@storybook/vue3'
+
+/** A spin button lets users type numbers and increase or decrease using icon buttons. */
+const meta: Meta<typeof FormSpinbutton> = {
+	title: 'Ui/Form/FormSpinbutton',
+	component: FormSpinbutton,
+	tags: ['autodocs'],
+	args: {
+		loading: false,
+		disabled: false,
+		size: 'md'
+	},
+	argTypes: {
+		size: {
+			control: 'select',
+			options: ['sm', 'md', 'lg']
+		}
+	},
+	render: (args) => ({
+		components: { FormSpinbutton },
+		setup() {
+			const modelValue = ref(0)
+			return { args, modelValue }
+		},
+		template: `
+        <div>
+          <FormSpinbutton v-bind="args" v-model="modelValue" />
+        </div>
+      `
+	})
+}
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {}
+
+export const Loading: Story = {
+	args: {
+		loading: true
+	}
+}
+
+export const Disabled: Story = {
+	args: {
+		disabled: true
+	}
+}
