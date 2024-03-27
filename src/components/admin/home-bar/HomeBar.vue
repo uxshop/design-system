5ec37334aa494e33117ecb5d41a0d14eaf7db7a5
Lucas Correa <lucasccorrea@gmail.com>
2024-03-27 09:32:58 -0300

    fix(HomeBar): class home-bar-title

--- src/components/admin/home-bar/HomeBar.vue
+++ src/components/admin/home-bar/HomeBar.vue
@@ -4,8 +4,8 @@ defineProps<{ userName?: string; message?: string }>()
 
 <template>
 	<div class="home-bar">
-		<h5 class="-title" v-if="userName">Olá, {{ userName }}!</h5>
-		<h5 class="-title" v-if="message">{{ message }}!</h5>
+		<h5 class="home-bar-title" v-if="userName">Olá, {{ userName }}!</h5>
+		<h5 class="home-bar-title" v-if="message">{{ message }}!</h5>
 		<slot name="content" />
 	</div>
 </template>
