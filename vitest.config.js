76ec4eb50893db72f5b537f10c09cfbc8330d305
Rafael Henrique de Andrade <rafael.andrade@tray.net.br>
2024-09-19 13:31:50 -0300

    refactor: updates dependencies and remove unnecessary one

---
+++ vitest.config.js
@@ -0,0 +1,21 @@
+import { fileURLToPath } from 'node:url';
+import { configDefaults, defineConfig, mergeConfig } from 'vitest/config';
+import viteConfig from './vite.config';
+
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      root: fileURLToPath(new URL('./', import.meta.url)),
+      globals: true,
+      environment: 'jsdom',
+      coverage: {
+        enabled: true,
+        include: ['src/**/*.{ts,vue}'],
+        exclude: [],
+        reporter: ['text', 'html'],
+      },
+      exclude: [...configDefaults.exclude, 'e2e/**'],
+    },
+  })
+);
