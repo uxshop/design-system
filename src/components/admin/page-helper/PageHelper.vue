a34cac8898e7b7b3fe5241ad74707704dfe9a76f
Rafael Henrique de Andrade <rafael.andrade@tray.net.br>
2024-09-27 17:15:29 -0300

    fix: solve problem related with TableList exports

--- src/components/admin/page-helper/PageHelper.vue
+++ src/components/admin/page-helper/PageHelper.vue
@@ -1,29 +1,25 @@
 <script setup lang="ts">
-import Icon from '../../ui/icon/Icon.vue'
+import Icon from '../../ui/icon/Icon.vue';
+import type { IPageHelper } from './types';
 
-export interface IPageHelper {
-	helperName: string
-	helperLink?: string
-}
-
-const props = defineProps<IPageHelper>()
+const props = defineProps<IPageHelper>();
 const emit = defineEmits<{
-	(event: 'onClickLink'): void
-}>()
+  (event: 'onClickLink'): void;
+}>();
 
 function onClickLink() {
-	emit('onClickLink')
-	if (props.helperLink) window.open(props.helperLink, 'blank')
+  emit('onClickLink');
+  if (props.helperLink) window.open(props.helperLink, 'blank');
 }
 </script>
 
 <template>
-	<div class="ui-page-helper" @click="onClickLink">
-		<icon name="info" filled size="24" />
-		<span>
-			<b>Precisa de ajuda?</b> Acesse o material sobre <b class="ui-page-helper-name"> {{ helperName }}</b>
-		</span>
-	</div>
+  <div class="ui-page-helper" @click="onClickLink">
+    <icon name="info" filled size="24" />
+    <span>
+      <b>Precisa de ajuda?</b> Acesse o material sobre <b class="ui-page-helper-name"> {{ helperName }}</b>
+    </span>
+  </div>
 </template>
 
 <style lang="scss">
