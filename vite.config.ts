3088c629036330bda92d42f3f5cdb9a92e859283
Rafael Henrique de Andrade <rafael.andrade@tray.net.br>
2024-10-16 09:48:53 -0300

    feat: prepare ds for bulding

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,36 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 
 export default defineConfig({
   plugins: [vue(), vueJsx()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+      },
+    },
+  },
+  build: {
+    lib: {
+      name: 'design-system',
+      formats: ['es'],
+      entry: {
+        index: resolve(__dirname, './src/index.ts'),
+        setup: resolve(__dirname, './src/setup.ts'),
+        filters: resolve(__dirname, './src/filters.ts'),
+        directives: resolve(__dirname, './src/directives.ts'),
+      },
+    },
+    copyPublicDir: false,
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
 });
