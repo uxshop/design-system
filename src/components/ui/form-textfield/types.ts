0d83575d34ab845ab30831ec8e6fca8c9644737d
Fabrício Caldana Anelli <fabricio.anelli@bagy.com.br>
2025-04-28 17:12:50 -0300

    refactor: melhoria na tipagem do value

--- src/components/ui/form-textfield/types.ts
+++ src/components/ui/form-textfield/types.ts
@@ -4,13 +4,13 @@ import type { FormWrapperProps } from '../form-wrapper';
 import type { FormWrapperEmits } from '../form-wrapper/types';
 
 export interface FormTextfieldEmits extends FormWrapperEmits {
-  (event: 'update', value: any): void;
+  (event: 'update', value: string | null): void;
   (e: 'focus', event: Event): void;
   (e: 'blur', event: Event): void;
   (e: 'keydown', event: Event): void;
   (e: 'keydownEnter', event: Event): void;
   (e: 'clear'): void;
-  (e: 'updateRaw', val: any): void;
+  (e: 'updateRaw', val: string | undefined): void;
 }
 
 export interface FormTextfieldProps extends FormWrapperProps {
